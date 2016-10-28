f2ac8205a2a8c33039df209dbd5fbe82d17aa6a6
William <wilcorrea@gmail.com>
2016-10-28 06:15:56 -0200

    Deploy

--- app/routes/nested/site.php
+++ app/routes/nested/site.php
@@ -4,14 +4,14 @@ use Simples\Core\Flow\Router;
 
 return function (Router $router) {
 
-    $router->get('/', function($data) use ($router) {
+    $router
+        ->get('/', function ($data) use ($router) {
 
-        return $router->response()->view('index.phtml', ['title' => 'SITE', 'menu' => $data['menu']]);
-    });
+            return $router->response()->view('index.phtml', ['title' => 'SITE', 'menu' => $data['menu']]);
+        })
+        ->get('/data', function ($data) {
 
-    $router->get('/data', function ($data) {
-
-        return $data;
-    });
+            return $data;
+        });
 
 };
