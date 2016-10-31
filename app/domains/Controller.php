0cbbf22e2ff2d6a50a9fe9989c1171736d9fe60a
William <wilcorrea@gmail.com>
2016-10-31 21:06:34 -0200

    Swagger support

--- app/domains/Controller.php
+++ app/domains/Controller.php
@@ -10,5 +10,14 @@ use Simples\Core\Flow\Controller as FlowController;
  */
 class Controller extends FlowController
 {
+    /**
+     * @SWG\Info(title="My First API", version="0.1")
+     */
 
+    /**
+     * @SWG\Get(
+     *     path="/api/resource.json",
+     *     @SWG\Response(response="200", description="An example resource")
+     * )
+     */
 }
